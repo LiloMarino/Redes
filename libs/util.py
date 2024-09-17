cb9131447594863fc0ead8113fc9bfe7f36d6962
LiloMarino <lilo.marino@hotmail.com>
2024-09-17 09:54:14 -0300

    Teste

--- libs/util.py
+++ libs/util.py
@@ -13,20 +13,17 @@ def receive_lost_packet(server_port: int) -> int:
             client_socket, ip_cliente = host_socket.accept()
             with client_socket:
                 data = client_socket.recv(500)
-                client_socket.close()
-            return int(data.decode())  # Assegure-se de converter para inteiro
+                return int(data.decode())
     except Exception as e:
         print(f"Erro ao receber pacotes perdidos: {e}")
         return -1  # Retorne um valor de erro apropriado
 
 
 def send_lost_packet(server_ip: str, server_port: int, lost_packets: int):
-    time.sleep(1)
+    time.sleep(3)
     try:
         with socket(AF_INET, SOCK_STREAM) as client_socket:
             client_socket.connect((server_ip, server_port))
-            client_socket.sendall(
-                str(lost_packets).encode()
-            )  # Envia como string codificada
+            client_socket.sendall(str(lost_packets).encode())
     except Exception as e:
         print(f"Erro ao enviar pacotes perdidos: {e}")
